33482572463fdb34d4146daf1298d9ea5e3bc5da
junhyoungkim <kkjn0131@naver.com>
2024-01-28 16:04:55 +0900

    api 데이터 갱신을 scheduler로 관리하기위해 servlet-context 설정 및 코드 작성

---
+++ src/main/java/com/ezen/royal/scheduler/Scheduler.java
@@ -0,0 +1,40 @@
+package com.ezen.royal.scheduler;
+
+import java.util.Calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.ezen.royal.service.OpenApiService;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Component
+public class Scheduler {
+	
+	@Autowired
+	OpenApiService apiService;
+	
+	/**
+	 * 1. 매일 오전 ?시에 api데이터를 갱신하는 스케쥴러
+	 */
+	// * 을 입력할경우 모두(항상)으로 설정함
+	// cron = 초 분 시 일 월 요일
+	@Scheduled(cron = "0 3 16 * * *")
+	public void autoUpdate() {
+		
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(cal.YEAR);
+		int month = cal.get(cal.MONTH) + 1;
+		int date = cal.get(cal.DATE);
+		int hour = cal.get(cal.HOUR_OF_DAY);
+		int min = cal.get(cal.MINUTE);
+		int sec = cal.get(cal.SECOND);
+		apiService.upsertApiDataList();
+		
+		log.info(String.format("[%d/%d/%d | %02d:%02d:%02d]api 데이터 최신화가 완료되었습니다", year, month, date, hour, min, sec));
+	}
+
+}
