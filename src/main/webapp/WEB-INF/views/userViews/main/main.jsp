a3fbbce6a2320d65b0e417b9e4040c67a6677258
hwangjinsol <ghkdwlsthf0112@naver.com>
2024-02-01 19:42:10 +0900

    메인페이지 슬라이더 수정 중

--- src/main/webapp/WEB-INF/views/userViews/main/main.jsp
+++ src/main/webapp/WEB-INF/views/userViews/main/main.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
@@ -11,42 +11,33 @@
 <link rel="stylesheet" href="${sliderCSS}" />
 </head>
 <body>
-	<jsp:include page="../../layout/header.jsp" />
-	<jsp:include page="../../layout/navibar.jsp" />
+   <jsp:include page="../../layout/header.jsp" />
+   <jsp:include page="../../layout/navibar.jsp" />
 
-	<div style="font-size: 40px;">궁능유적본부</div>
-	<div class="box">
-		<div>
-			<button class="back">❮</button>
-		</div>
-		<div class="slide">
-			<div class="images">
-				<img class="img" alt="경복궁"
-					src="../resources/image/royal_inner/main/gyeongbok/gyeongbokgung.jpg" />
-				<img class="img" alt="덕수궁"
-					src="../resources/image/royal_inner/main/deoksu/deoksugung.jpg" />
-				<img class="img" alt="창경궁"
-					src="../resources/image/royal_inner/main/changgyeong/changgyeonggung.jpg" />
-				<img class="img" alt="창덕궁"
-					src="../resources/image/royal_inner/main/changdeok/changdeokgung.jpg" />
-				<img class="img" alt="종묘"
-					src="../resources/image/royal_inner/main/jongmyo/jongmyo.jpg" />
-			</div>
-		</div>
-		<div>
-			<button class="next">❯</button>
-		</div>
-	</div>
-	<div>
-		공지사항 <a href="">전체보러가기</a>
-		<div>리스트1,2,3</div>
-	</div>
-	<div>
-		<a href="/royal/palace/event">알림판(행사)</a>
-	</div>
-	<!-- 행사 가는 링크 달기 -->
-	<jsp:include page="../../layout/footer.jsp" />
+   <div style="font-size: 40px;">궁능유적본부</div>
+   <div class="box">
+      <div class="slide">
+         <div class="images">
+            <img alt="경복궁" src="../resources/image/royal_inner/main/gyeongbok/gyeongbokgung.jpg" />
+            <img alt="덕수궁" src="../resources/image/royal_inner/main/deoksu/deoksugung.jpg" />
+            <img alt="창경궁" src="../resources/image/royal_inner/main/changgyeong/changgyeonggung.jpg" />
+            <img alt="창덕궁" src="../resources/image/royal_inner/main/changdeok/changdeokgung.jpg" />
+            <img alt="종묘" src="../resources/image/royal_inner/main/jongmyo/jongmyo.jpg" />
+         </div>
+      </div>
+      <button class="back">❮</button>
+      <button class="next">❯</button>
+   </div>
+   <div>
+      공지사항 <a href="">전체보러가기</a>
+      <div>리스트1,2,3</div>
+   </div>
+   <div>
+      <a href="/royal/palace/event">알림판(행사)</a>
+   </div>
+   <!-- 행사 가는 링크 달기 -->
+   <jsp:include page="../../layout/footer.jsp" />
 
-	<script src="${sliderJS}"></script>
+   <script src="${sliderJS}"></script>
 </body>
 </html>
--- src/main/webapp/resources/js/usermainviews/slider.js
+++ src/main/webapp/resources/js/usermainviews/slider.js
@@ -1,91 +1,21 @@
-// window.onload = function() {
-//     const kindWrap =  document.querySelector('.kind_wrap');
-//     const slider = kindWrap.querySelector('.slider');
-//     const slideLis = slider.querySelectorAll('.img')
-//     const moveButton = kindWrap.querySelector('.arrow');
-
-//     /* 클론 */
-//     const clone1 = slideLis[0].cloneNode(true);
-//     const cloneLast = slideLis[slideLis.length - 1].cloneNode(true);
-//     slider.insertBefore(cloneLast, slideLis[0]);
-//     slider.appendChild(clone1);
-
-//     /* 주요 변수 초기화 */  
-//     let currentIdx = 0;
-//     let translate = 0;
-//     const speedTime = 500;
-
-//     /* CSSOM 셋업 */
-//     const sliderCloneLis = slider.querySelectorAll('.img');
-//     const liWidth = slideLis[0].clientWidth;
-//     const sliderWidth = liWidth * sliderCloneLis.length;
-//     slider.style.width = `${sliderWidth}px`;
-//     currentIdx = 1;
-//     translate = -liWidth;
-//     slider.style.transform = `translateX(${translate}px)`
-
-//     /* 리스너 설치하기 */
-//     moveButton.addEventListener('click', moveSlide);
-
-//     /* 슬라이드 실행 */
-//     function move(D) {
-//       currentIdx += (-1 * D);
-//       translate += liWidth * D;
-//       slider.style.transform = `translateX(${translate}px)`;
-//       slider.style.transition = `all ${speedTime}ms ease`
-//     }
-
-//     /* 클릭 버튼 */
-//     function moveSlide(event) {
-//         event.preventDefault();
-//         if (event.target.className === 'next') {
-//             move(-1);
-//             if (currentIdx === sliderCloneLis.length - 1) {
-//                 setTimeout(() => {
-//                     slider.style.transition = 'none';
-//                     currentIdx = 1;
-//                     translate = -liWidth;
-//                     slider.style.transform = `translateX(${translate}px)`;
-//                     setTimeout(() => {
-//                         slider.style.transition = `transform ${speedTime}ms ease`;
-//                     }, 50); // 추가된 부분
-//                 }, speedTime);
-//             }
-//       } else {
-//         move(1);
-//         if (currentIdx === 0) {
-//             setTimeout(() => {
-//                 slider.style.transition = 'none';
-//                 currentIdx = sliderCloneLis.length - 2;
-//                 translate = -(liWidth * currentIdx);
-//                 slider.style.transform = `translateX(${translate}px)`;
-//                 setTimeout(() => {
-//                     slider.style.transition = `transform ${speedTime}ms ease`;
-//                 }, 50); // 추가된 부분
-//             }, speedTime);
-//           }
-//         }
-//     }
-
-//   }
 
 let pages = 0;//현재 인덱스 번호
 let positionValue = 0;//images 위치값
-const IMAGE_WIDTH = 250;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
+const IMAGE_WIDTH = 1000;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
 //DOM
 const backBtn = document.querySelector(".back")
 const nextBtn = document.querySelector(".next")
 const images = document.querySelector(".images")
 
 function next() {
-  if (pages< 2) {
+  if (pages< 4) {
     backBtn.removeAttribute('disabled')//뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
     positionValue -= IMAGE_WIDTH;//IMAGE_WIDTH의 증감을 positionValue에 저장한다.
     images.style.transform = `translateX(${positionValue}px)`;
 		//x축으로 positionValue만큼의 px을 이동한다.
     pages += 1; //다음 페이지로 이동해서 pages를 1증가 시킨다.
   }
-  if (pages === 2) { //
+  if (pages === 4) { //
     nextBtn.setAttribute('disabled', 'true')//마지막 장일 때 next버튼이 disabled된다.
   }
 }
