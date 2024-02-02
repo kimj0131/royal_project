4e2913ff5720284ab7ea7f97da3b6c5f3dd4e66b
junhyoungkim <kkjn0131@naver.com>
2024-02-02 11:16:54 +0900

    ✅ naver 로그인 - member데이터 처리과정 업데이트 > callback 페이지에서 비동기로 데이터를 처리하는 과정 -> 페이지를 forward해 데이터를 처리

--- src/main/java/com/ezen/royal/api/service/NaverServiceImpl.java
+++ src/main/java/com/ezen/royal/api/service/NaverServiceImpl.java
@@ -12,6 +12,7 @@ public class NaverServiceImpl implements NaverService{
 	@Autowired
 	LoginMapper loginMapper;
 	
+	// 네아로 api 서비스를 활용해 받은 데이터를 DB에 저장 및 업데이트하는 메서드
 	@Override
 	public int upsertMember(MemberDTO dto) {
 		return loginMapper.upsertMember(dto);
