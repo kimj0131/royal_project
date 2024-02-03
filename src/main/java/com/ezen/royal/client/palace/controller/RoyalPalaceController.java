80fbd5050edac7bd27696a864adab102c7beec4a
hyemi-bang <pbhm1124@gmail.com>
2024-02-03 12:04:20 +0900

    royalPalaceController 전페이지 나열 -> 궁 이름 변수로 가정하여 코드 짧게 수정 gyeongbokHistory 파일명수정

--- src/main/java/com/ezen/royal/client/palace/controller/RoyalPalaceController.java
+++ src/main/java/com/ezen/royal/client/palace/controller/RoyalPalaceController.java
@@ -2,6 +2,7 @@ package com.ezen.royal.client.palace.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -10,60 +11,15 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @RequestMapping("/palace")
 public class RoyalPalaceController {
+	
+	@GetMapping("/{palaceName}main")
+    public String palaceMain(@PathVariable String palaceName) {
+        // palaceName을 변수로 가정하고, 이를 사용하여 페이지를 동적으로 로드
+        return "/userViews/royal_intro/" + palaceName + "Palace/" + palaceName + "Main";
+    }
 
-/////////////창덕궁
-	@GetMapping("/changdeokmain")
-	public String changdeok() {
-		return "/userViews/royal_intro/changdeokPalace/changdeokMain";
-	}
-
-	@GetMapping("/changdeokhistory")
-	public String changdeokHistory() {
-		return "/userViews/royal_intro/changdeokPalace/changdeokHistory";
-	}
-
-/////////////창경궁
-	@GetMapping("/changgyeongmain")
-	public String changgyeong() {
-		return "/userViews/royal_intro/changgyeongPalace/changgyeongMain";
-	}
-
-	@GetMapping("/changgyeonghistory")
-	public String chnggyeongHistory() {
-		return "/userViews/royal_intro/changgyeongPalace/changgyeongHistory";
-	}
-
-/////////////덕수궁
-	@GetMapping("/deoksumain")
-	public String deoksu() {
-		return "/userViews/royal_intro/deoksuPalace/deoksuMain";
-	}
-
-	@GetMapping("/deoksuhistory")
-	public String deoksuHistory() {
-		return "/userViews/royal_intro/deoksuPalace/deoksuHistory";
-	}
-
-/////////////경복궁
-	@GetMapping("/gyeongbokmain")
-	public String gyeongbok() {
-		return "/userViews/royal_intro/gyeongbokPalace/gyeongbokMain";
-	}
-
-	@GetMapping("/gyeongbokhistory")
-	public String gyeongbokHistory() {
-		return "/userViews/royal_intro/gyeongbokPalace/gyeonbokHistory";
-	}
-
-/////////////종묘
-	@GetMapping("/jongmyomain")
-	public String jongmyo() {
-		return "/userViews/royal_intro/jongmyoPalace/jongmyoMain";
-	}
-
-	@GetMapping("/jongmyohistory")
-	public String jongmyoHistory() {
-		return "/userViews/royal_intro/jongmyoPalace/jongmyoHistory";
-	}
-
+    @GetMapping("/{palaceName}history")
+    public String palaceHistory(@PathVariable String palaceName) {
+        return "/userViews/royal_intro/" + palaceName + "Palace/" + palaceName + "History";
+    }
 }
