16db35f8e592bbd01e4cfb0fc0bd809765adb695
hwangjinsol <ghkdwlsthf0112@naver.com>
2024-02-05 14:50:49 +0900

    ✅메인페이지 슬라이드 수정 완료, CSS위치 변경

--- src/main/webapp/WEB-INF/views/userViews/main/main.jsp
+++ src/main/webapp/WEB-INF/views/userViews/main/main.jsp
@@ -6,6 +6,10 @@
 <head>
 <c:url value="/resources/css/usermainviews/slider.css" var="sliderCSS" />
 <c:url value="/resources/js/usermainviews/slider.js" var="sliderJS" />
+<c:url value="/resources/image/slider/event1.jpg" var="banner1" />
+<c:url value="/resources/image/slider/event2.jpg" var="banner2" />
+<c:url value="/resources/image/royal_inner/main/jongmyo/jongmyo.jpg"
+	var="jongmyo" />
 <meta charset="UTF-8">
 <title>메인페이지</title>
 <link rel="stylesheet" href="${sliderCSS}" />
@@ -15,26 +19,48 @@
 	<jsp:include page="/WEB-INF/views/layout/navibar.jsp" />
 	<div class="title" style="font-size: 30px;">궁능유적본부</div>
 
-
-	<div class="images">
-		<div class="img"></div>
-		<div class="img"></div>
+	<div class="outer">
+		<div class="inner-list">
+			<div class="inner">
+				<img alt="수문장 교대의식" src="${banner1}">
+			</div>
+			<div class="inner">
+				<img alt="궁능유적본부" src="${banner2}">
+			</div>
+			<div class="inner">
+				<img alt="종묘" src="${jongmyo}">
+			</div>
+			<div class="button1">
+				<a href="#" class="back" title="이전이미지">back</a> <a href="#"
+					class="next" title="다음이미지">next</a>
+			</div>
+		</div>
 	</div>
-	<div class="button1">
-		<a href="#" class="back" title="이전이미지">back</a> <a href="#"
-			class="next" title="다음이미지">next</a>
+	<div class="layout">
+		<div class="notice">
+			<div class="title_top">
+				<h2 class="notice_title">공지사항</h2>
+				<a href=""> <span class="all">전체보러가기</span>
+				</a>
+			</div>
+			<ul class="list">
+				<li class="item"><a href=""> <span class="inner_title">공지사항</span>
+						<div>공지사항 제목 내용1111111</div> <span class="inner_title">2024.02.05</span>
+				</a></li>
+				<li class="item"><a href=""> <span class="inner_title">공지사항</span>
+						<div>공지사항 제목 내용2222222</div> <span class="inner_title">2024.02.05</span>
+				</a></li>
+				<li class="item"><a href=""></a></li>
+				<li class="item"><a href=""></a></li>
+			</ul>
+		</div>
+		<div>
+			<div class="inform">
+				<a href="/royal/palace/event">알림판(행사)</a>
+			</div>
+			<!-- 행사 가는 링크 달기 -->
+		</div>
 	</div>
-
-
-
-	<div class="notice">
-		공지사항 <a href="">전체보러가기</a>
-		<div>리스트1,2,3</div>
-	</div>
-	<div>
-		<a href="/royal/palace/event">알림판(행사)</a>
-	</div>
-	<!-- 행사 가는 링크 달기 -->
 	<jsp:include page="/WEB-INF/views/layout/footer.jsp" />
 
 	<script src="${sliderJS}"></script>
--- src/main/webapp/resources/js/usermainviews/slider.js
+++ src/main/webapp/resources/js/usermainviews/slider.js
@@ -1,57 +1,59 @@
+const outer = document.querySelector('.outer');
+const innerList = document.querySelector('.inner-list');
+const inners = document.querySelectorAll('.inner');
+let currentIndex = 0; // 현재 슬라이드 화면 인덱스
+const IMAGE_WIDTH = 1214; // 한번 이동 시 IMAGE_WIDTH만큼 이동한다.
 
-let pages = 0;//현재 인덱스 번호
-let positionValue = 0;//images 위치값
-const IMAGE_WIDTH = 1214;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
-//DOM
-const backBtn = document.querySelector(".back");
-const nextBtn = document.querySelector(".next");
-const images = document.querySelector(".images");
+function adjustSlideSize() {
+  inners.forEach((inner) => {
+    inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+  });
 
-function next() {
-  if (pages< 1) {
-    backBtn.removeAttribute('disabled');//뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
-    positionValue -= IMAGE_WIDTH;//IMAGE_WIDTH의 증감을 positionValue에 저장한다.
-    images.style.transform = `translateX(${positionValue}px)`;
-		//x축으로 positionValue만큼의 px을 이동한다.
-    pages += 1; //다음 페이지로 이동해서 pages를 1증가 시킨다.
-  }
-  if (pages === 1) { //
-    nextBtn.setAttribute('disabled', 'true');//마지막 장일 때 next버튼이 disabled된다.
-  } else {
+  innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
 
-  }
+  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // 슬라이드 위치도 조절
 }
 
-function back() {
-  if (pages > 0) {
-    nextBtn.removeAttribute('disabled');
-    positionValue += IMAGE_WIDTH;
-    images.style.transform = `translateX(${positionValue}px)`;
-    pages -= 1; //이전 페이지로 이동해서 pages를 1감소 시킨다.
-  }
-  if (pages === 0) {
-    backBtn.setAttribute('disabled', 'true');//마지막 장일 때 back버튼이 disabled된다.
-  }
+// 창 크기가 변경될 때 슬라이드 크기를 조절하는 이벤트 추가
+window.addEventListener('resize', adjustSlideSize);
+
+// 초기화
+adjustSlideSize();
+
+/*
+  버튼에 이벤트 등록하기
+*/
+const buttonLeft = document.querySelector('.back');
+const buttonRight = document.querySelector('.next');
+
+buttonLeft.addEventListener('click', () => {
+  currentIndex--;
+  currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
+  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+  resetInterval(); // setInterval 재설정
+});
+
+buttonRight.addEventListener('click', () => {
+  currentIndex++;
+  currentIndex = currentIndex >= inners.length ? 0 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 첫 번째로 초기화
+  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+  resetInterval(); // setInterval 재설정
+});
+
+/*
+  주기적으로 화면 넘기기
+*/
+let interval = getInterval(); // interval 등록
+
+function getInterval() {
+  return setInterval(() => {
+    currentIndex++;
+    currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+  }, 4000);
 }
 
 function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(next, 4000);
+  clearInterval(interval); // 기존 동작되던 interval 제거
+  interval = getInterval(); // 새로운 interval 등록
 }
-
-function init() {  //초기 화면 상태
-  backBtn.setAttribute('disabled', 'true'); //속성이 disabled가 된다.
-  backBtn.addEventListener("click", () => {
-    back();
-    resetInterval();
-  });  //클릭시 다음으로 이동한다.
-
-  nextBtn.addEventListener("click", () => {
-    next();
-    resetInterval();
-  });//클릭시 이전으로 이동한다.
-}
-init();
-
-
-let interval = setInterval(next, 4000);
