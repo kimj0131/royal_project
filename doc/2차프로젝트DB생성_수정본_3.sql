4f0cc0f1d3e8c21c2bf9cad902dadabed46d4f11
junhyoungkim <kkjn0131@naver.com>
2024-02-06 14:45:04 +0900

    📝 프로젝트 관련 파일 추가

---
+++ doc/2차프로젝트DB생성_수정본_3.sql
@@ -0,0 +1,269 @@
+--------------------------------------------------------------------------------
+-- Oracle 새접속
+
+-- NAME  : 2차프로젝트
+-- 사용자 이름 : royal
+-- 비밀번호 : 
+-------------------------------------------------------------------------------
+-- 만들어진 royal 계정 접속 후 실행 
+
+-- 필요시 테이블 명 혹은 시퀀스 명 맞춰서 사용 
+-- SELECT * FROM tabs; --계정 내 테이블 정보 확인
+-- SELECT * FROM user_constraints; -- 제약조건 확인 
+-- SELECT * FROM user_sequences; -- 사용자 설정 시퀀스 확인
+
+
+-- 1. 궁(royal)테이블 생성
+CREATE TABLE royal(
+    royal_id number(2) --시퀀스
+    CONSTRAINT royal_id_pk PRIMARY KEY,
+    royal_name VARCHAR2(15) -- 궁 이름
+    CONSTRAINT royal_name_nn NOT NULL
+);
+
+-- 궁(royal)테이블 시퀀스 생성
+CREATE SEQUENCE royal_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+
+-- SELECT * FROM royal;
+ --DROP TABLE royal;
+-- DROP SEQUENCE royal_id_seq;
+-- TRUNCATE TABLE royal;                         
+              
+
+
+
+
+-- 2. 궁 내부 부속 건축물 테이블 (공공API데이터 담아둘 테이블)
+CREATE TABLE royal_inner (
+    royal_inner_id NUMBER(3)  --  시퀀스
+    CONSTRAINT royal_inner_id_pk Primary Key,
+    royal_id NUMBER(2) -- 궁 시퀀스 (royal table과 join 사용)
+    CONSTRAINT royal_id_fk REFERENCES royal(royal_id),
+    royal_inner_name VARCHAR2(50)  -- 건축물 명
+    CONSTRAINT royal_inner_name_nn NOT NULL,
+    royal_inner_detail VARCHAR2(4000), -- 건축물 상세 설명
+    royal_inner_imgpath VARCHAR2(4000) -- 건축물 이미지 경로
+);
+
+                     
+-- SELECT * FROM royal_inner;
+-- DROP TABLE royal_inner;
+-- DROP SEQUENCE royal_inner_id_seq;
+-- TRUNCATE TABLE royal_inner;                         
+              
+                     
+-- 3. 소셜 로그인 유저 목록 테이블
+CREATE TABLE member(
+    member_id NUMBER(5)  -- 유저 시퀀스
+    CONSTRAINT member_id_pk PRIMARY KEY,
+    social_id VARCHAR2(200) -- 소셜 식별id
+    CONSTRAINT social_id_nn NOT NULL,
+    member_name VARCHAR2(50)  --회원 이름
+    CONSTRAINT member_name_nn NOT NULL,
+    member_email VARCHAR2(50)  -- 회원 이메일
+    CONSTRAINT member_email_nn NOT NULL,
+    member_type VARCHAR2(10)  --네이버/카카오
+    CONSTRAINT member_type_nn NOT NULL -- 
+);
+CREATE SEQUENCE member_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+-- SELECT * FROM member;
+ --DROP TABLE member;
+-- DROP SEQUENCE member_id_seq;
+-- TRUNCATE TABLE member;                         
+
+
+
+ 
+-- 4. 공지사항 테이블                  
+CREATE TABLE notice(
+    notice_id NUMBER(10) --공지사항 시퀀스
+    CONSTRAINT notice_id_pk PRIMARY KEY,
+    royal_id number(2) -- 궁 번호
+    CONSTRAINT notice_royal_id_fk REFERENCES royal(royal_id),
+    notice_title VARCHAR2(100) --공지사항 제목
+    CONSTRAINT notice_title_nn NOT NULL, 
+    notice_content VARCHAR2(4000) --공지 내용 
+    CONSTRAINT notice_content_nn NOT NULL,
+    notice_date DATE  DEFAULT sysdate -- 공지 등록일
+);
+
+-- 공지사항 시퀀스 생성
+CREATE SEQUENCE notice_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+          
+-- SELECT * FROM notice;
+-- DROP TABLE notice;
+-- DROP SEQUENCE notice_id_seq;
+-- TRUNCATE TABLE notice;
+
+
+-- 5. 행사 목록 테이블
+CREATE TABLE event_list(
+    event_id NUMBER(5) -- 행사리스트 시퀀스
+    CONSTRAINT event_id_pk PRIMARY KEY,
+    royal_id  NUMBER(2) -- royal테이블 JOIN
+    CONSTRAINT event_royal_id_fk  REFERENCES royal(royal_id),
+    event_type VARCHAR2(25) -- 해설/ 행사
+    CONSTRAINT event_type_nn NOT NULL,   -- 해설 :commentary 행사: event 
+    event_name VARCHAR2(200) --행사명 
+    CONSTRAINT event_name_nn NOT NULL,
+    event_location VARCHAR2(100) -- 행사장소 
+    CONSTRAINT event_location_nn NOT NULL,
+    event_rounds NUMBER(2) DEFAULT 1  -- 총 회차 수
+    CONSTRAINT event_rounds_nn NOT NULL,
+    start_date DATE --행사 시작일
+    CONSTRAINT start_date_nn  NOT NULL,
+    end_date DATE --행사 종료일
+    CONSTRAINT end_date_nn  NOT NULL,
+    event_link VARCHAR2(1000) --행사관련사이트 링크
+    CONSTRAINT event_link_nn NOT NULL , 
+    event_imgpath VARCHAR2(500)
+    CONSTRAINT event_imgpath_nn NOT NULL ,
+    reservable CHAR(2) -- 예약가능 여부
+    CONSTRAINT reservable_nn  NOT NULL
+);
+
+--행사리스트 시퀀스 생성
+CREATE SEQUENCE eventlist_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+
+-- SELECT * FROM eventlist;
+-- DROP TABLE eventlist;
+-- DROP SEQUENCE eventlist_id_seq;
+-- TRUNCATE TABLE eventlist;
+
+
+
+-- 6. 행사 회차 테이블
+CREATE TABLE event_round(
+    round_id NUMBER(5) -- 행사 회차 번호
+    CONSTRAINT round_id_pk PRIMARY KEY,
+    event_id NUMBER(5) -- 행사 번호 (FK)
+    CONSTRAINT event_round_event_id_fk REFERENCES event_list(event_id),
+    round_num NUMBER(2)  -- 회차
+    CONSTRAINT round_num_nn NOT NULL,
+    round_time VARCHAR2(20)  -- 회차 별 시간
+    CONSTRAINT round_time_nn NOT NULL,
+    round_capacity NUMBER(3) 
+    CONSTRAINT round_capacity_nn NOT NULL
+    CONSTRAINT round_capacity_chk  CHECK(round_capacity >=0)
+);
+
+-- 행사 회차 시퀀스 생성
+CREATE SEQUENCE event_round_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+-- SELECT * FROM event_round;
+-- DROP TABLE event_round;
+-- DROP SEQUENCE event_round_id_seq_id_seq;
+-- TRUNCATE TABLE event_round;
+
+
+          
+-- 7. 예약 테이블
+CREATE TABLE reservation ( 
+    resv_id NUMBER(5) -- 예약 id
+    CONSTRAINT resv_id_pk Primary Key,
+    event_id NUMBER(5) -- 이벤트 번호
+    CONSTRAINT event_id_fk REFERENCES event_list(event_id),--(fk)
+    member_id NUMBER(5) -- 유저 번호 (식별id 아님)
+    CONSTRAINT member_id_fk REFERENCES member(member_id), --(fk)
+    resv_num VARCHAR2(100) -- 예약번호 (문자(e, c) + 숫자(240206 + 000000(난수))
+    CONSTRAINT resv_num_nn NOT NULL
+    CONSTRAINT resv_num_uq UNIQUE,
+    resv_date DATE  -- 예약 날짜
+    DEFAULT sysdate, --(date)
+    resv_people NUMBER(5) -- 예약한 인원
+    CONSTRAINT resv_people_nn NOT NULL
+    CONSTRAINT resv_people_chk  CHECK(resv_people >=0)
+);
+
+-- SELECT * FROM reservation;
+ --DROP TABLE reservation;
+-- DROP SEQUENCE reservation_id_seq;
+-- TRUNCATE TABLE reservation;
+
+
+-- 예약 시퀀스 생성
+CREATE SEQUENCE resv_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                     NOCYCLE -- 숫자 순환 여부 설정
+                     NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+
+
+
+
+-- 8. Q&A 테이블
+CREATE TABLE qna (
+    qna_id NUMBER(10) -- qna 번호
+    CONSTRAINT qna_id_pk PRIMARY KEY, 
+    member_id NUMBER(5) -- 유저 번호 (시퀀스)
+    CONSTRAINT qna_member_id_fk REFERENCES member(member_id), 
+    royal_id NUMBER(2) -- 궁 번호
+    CONSTRAINT qna_royal_id_fk REFERENCES royal(royal_id),
+    qna_title VARCHAR2(2000) -- qna 제목
+    CONSTRAINT qna_title_nn NOT NULL,
+    qna_content VARCHAR2(2000) -- qna 내용
+    CONSTRAINT qna_content_nn NOT NULL,
+    qna_result VARCHAR2(2000), -- qna 답변내용
+    qna_date DATE  DEFAULT SYSDATE  -- qna 작성일
+);
+
+
+--qna 시퀀스
+ CREATE SEQUENCE qna_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                    NOCYCLE -- 숫자 순환 여부 설정
+                    NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+
+
+
+-- 9. 자주하는 질문 테이블
+CREATE TABLE faq(
+    faq_id NUMBER(5) -- faq 번호(시퀀스)
+    CONSTRAINT faq_id_pk PRIMARY KEY,
+    royal_id NUMBER(2) -- 궁 번호
+    CONSTRAINT faq_royal_id_fk REFERENCES royal(royal_id), 
+    faq_title VARCHAR2(100) -- faq 제목
+    CONSTRAINT faq_title_nn NOT NULL,
+    faq_result VARCHAR2(4000) -- faq 내용
+    CONSTRAINT faq_result_nn NOT NULL
+);
+ 
+ -- 자주하는질문 시퀀스
+CREATE SEQUENCE faq_id_seq
+                    START WITH 1 -- 시작 번호 설정
+                    INCREMENT BY 1  -- 증가 숫자 설정
+                    NOCYCLE -- 숫자 순환 여부 설정
+                    NOCACHE; -- 번호를 미리 만들어놓을 양을 설정
+
+
+-- 계정 내 모든 테이블 보기
+--select * from tabs;
+--
+---- 제약조건 보기 
+--select * from user_constraints;
+--
+--
+--select * from user_sequences;
+--drop sequence royal_inner_seq;
+
+
---
+++ doc/eventlist.sql
@@ -0,0 +1,350 @@
+--1
+
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '경복궁 수문장 교대의식', '경복궁 광화문', 1, '2024-01-01', '2024-12-31', 
+'https://www.chf.or.kr/short/6d6N',
+'https://www.chf.or.kr/jnrepo/namo/img/images/000063/20240119205704416_TEOZXDIK.png', 'N'
+);
+
+--2
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT  r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2024 갑진년 수문장 세화 나눔', '경복궁 광화문 수문장청', 2, '2024-02-09', '2024-02-10', 'https://www.chf.or.kr/short/tYMO',
+'https://www.chf.or.kr/jnrepo/namo/img/images/000064/20240203165553950_T0EWYCEJ.png', 'N'
+);
+
+--3
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '덕수궁'),
+'행사', '2023년 밤의 석조전(하반기)', '덕수궁 석조전', 3, '2023-10-06', '2023-11-02',
+'https://www.chf.or.kr/short/hEqy',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/1db199e8ed9e48bbb0b0c77d740564bc_1694366002947.png', 'Y'
+);
+
+--4
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 궁궐 호위군 사열의식 첩종 疊鐘', '경복궁 흥례문 앞', 2, '2023-10-20', '2023-10-22',
+'https://www.chf.or.kr/short/fi9u',
+'https://www.chf.or.kr/jnrepo/namo/img/images/000059/20231016142117927_VYVR1LM3.png', 'N'
+);
+
+--5
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '[2023 가을 궁중문화축전] 아티스트가 사랑한 궁', '경복궁 집옥재', 1, '2023-10-17', '2023-10-18',
+'https://www.chf.or.kr/short/2VQA',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202310/bff36b4e177641d3974556b9265c0e60_1696470637572.jpg', 'Y'
+);
+
+--6
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창경궁'),
+'행사', '[2023 가을 궁중문화축전] 인문학 콘서트 창경궁, 우리의 유산', '창경궁 통명전', 1, '2023-10-16', '2023-10-17',
+'https://www.chf.or.kr/short/gVMn',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202310/2f968e90d66244b89b151759a4634bbe_1696470252224.jpg', 'Y'
+);
+
+--7
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '[2023 가을 궁중문화축전] 판소리로 듣는 궁 이야기', '경복궁 흥복전', 2, '2023-10-13', '2023-10-14', 'https://www.chf.or.kr/short/w7L3',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202310/20d08296e50a41e89135178ccc533490_1696469683972.jpg', 'Y'
+);
+
+--2023 가을 궁중문화축전(경복궁, 창덕궁, 덕수궁, 창경궁, 온라인)
+
+--8
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '덕수궁'),
+'행사', '2023 대한제국 외국공사 접견례', '덕수궁', 2, '2023-10-20', '2023-10-22',
+'https://www.chf.or.kr/short/NqMW',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202310/e462f2f5db5f41cab98651a4f3691df3_1696398974810.png', 'Y'
+);
+
+--9
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '궁궐일상모습 재현 및 체험-왕가의 산책', '경복궁 일원', 2, '2023-10-08', '2023-10-13',
+'https://www.chf.or.kr/short/gjG9',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/b6e718f676a6421298b54a6e9e21cca0_1695724417513.png', 'N'
+);
+
+--10
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 수라간 시식공감(하반기)', '경복궁', 1, '2023-10-20', '2023-10-28', 
+'https://www.chf.or.kr/short/ufnM',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/e9349b2c99c8416d847cf6abaca44c28_1695603370640.png', 'Y'
+);
+
+--11
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 고궁음악회 - 풍류에 그루브를 더하다', '경복궁 수정전', 1, '2023-10-13', '2023-10-20', 
+'https://www.chf.or.kr/short/9a0j',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/059a34e60f6f496d997fd34a378578a7_1695276566884.png', 'N'
+);
+
+--12
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 고궁음악회 - 발레x수제천', '경복궁 집옥재', 1, '2023-10-12', '2023-10-14', 
+'https://www.chf.or.kr/short/A2GE',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/8977791fe3ea479dafbd0281763b31da_1695254804985.jpg', 'Y'
+);
+
+--13
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창덕궁'),
+'행사', '사회적 배려 대상자 초청 ‘창덕궁 달빛기행’ 모집(연장)', '창덕궁', 2, '2023-10-26', '2023-10-27', 
+'https://www.chf.or.kr/short/CYjs',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/8327e8b3681244f98167910ff847bd65_1694999401278.png', 'Y'
+);
+
+--14
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '사회적 배려 대상자 초청 ‘경복궁 시식공감’ 모집', '경복궁', 2, '2023-10-23', '2023-10-28', 
+'https://www.chf.or.kr/short/Rnnb',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202309/743b5a3959d942ad832bae507cf86b1e_1694366833538.png', 'Y'
+);
+
+--15
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 경복궁 생과방(하반기)', '경복궁 생과방', 4, '2023-09-07', '2023-10-21',
+'https://www.chf.or.kr/short/8sQs',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202308/9324b656ff8b4f5b80303459514fa0bb_1693359368272.jpg', 'Y'
+);
+
+--16
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '사회적 배려 대상자 초청 ‘경복궁 별빛야행’ 모집', '경복궁', 2, '2023-10-05', '2023-10-07',
+'https://www.chf.or.kr/short/XBjK',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202308/dc85b797dc4d4283a90d035697909893_1693194291588.png', 'Y'
+);
+
+--17
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 경복궁 별빛야행(하반기)', '경복궁', 2, '2023-09-08', '2023-10-08',
+'https://www.chf.or.kr/short/BUoz',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202308/9a4032d4b60f4086b0b5b8ee76893c26_1692666774979.png', 'Y'
+);
+
+--18
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창덕궁'),
+'행사', '2023년 창덕궁 달빛기행(하반기)', '창덕궁', 2, '2023-09-07', '2023-10-22', 
+'https://www.chf.or.kr/short/2fbw',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202308/ce2da9099edc46a4ac6c6cb006830403_1692231253807.jpg', 'Y'
+);
+
+--19
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 수라간 시식공감', '경복궁', 1, '2023-05-25', '2023-06-04',
+'https://www.chf.or.kr/short/bEHk',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202305/2a989c9ee261463c9daec5c5a629ab02_1683507634941.png', 'Y'
+);
+
+--20
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 경복궁 생과방 - 경복궁에서 마주한 특별한 시간', '경복궁 생과방', 4, '2023-04-20', '2023-06-28',
+'https://www.chf.or.kr/short/ahQV',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/5847036fb7f442b9b1005aec127a07ba_1680670289132.png', 'Y'
+);
+
+--21
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창덕궁'),
+'행사', '2023년 창덕궁 달빛기행', '창덕궁', 2, '2023-04-13', '2023-06-04',
+'https://www.chf.or.kr/short/OQJG',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/2572601b38b7494eba93a1bb999bad8a_1680832352792.jpg', 'Y'
+);
+
+--22
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '종묘'),
+'행사', '2023 종묘대제', '종묘 영녕전 및 종묘 일원', 1, '2023-05-07', '2023-05-07',
+'https://www.chf.or.kr/short/dD05',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/c38362b37b6343c1979c8fff900c51a0_1682571270286.jpg', 'Y'
+);
+
+--23
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 수문장 교대의식 어린이날 특별행사', '경복궁 협생문 밖 공개훈련장 일원', 2, '2023-05-05', '2023-05-05', 'https://www.chf.or.kr/short/vdV2',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/c3f32e0b7464404687faf97f3f015049_1682560123870.jpg', 'N'
+);
+
+--24
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '덕수궁'),
+'행사', '[고궁음악회] 풍류에 힙(Hip)을 더하다', '덕수궁 즉조당 앞마당', 1, '2023-05-05', '2023-05-28',
+'https://www.chf.or.kr/short/6cJB',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/f1bff7bd3a514b27b306dbb4ca6eb1e2_1682388103781.png', 'N'
+);
+
+--25
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '[고궁음악회] 풍류에 그루브(Groove)를 더하다', '경복궁 수정전', 2, '2023-04-29', '2023-05-06', 'https://www.chf.or.kr/short/bqR3',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/e942c3f22de74d66b04f868c7e6e6817_1682386993202.png', 'N'
+);
+
+--26
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '덕수궁'),
+'행사', '[궁중문화축전] 오얏꽃 개화만리', '덕수궁 정관헌', 3, '2023-05-04', '2023-05-06',
+'https://www.chf.or.kr/short/WOSz',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/d18c344c793b4321ba71eea45893339b_1681920717590.png', 'Y'
+);
+
+--27
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '종묘'),
+'행사', '2023년 종묘 묘현례 - 1703 조선의 왕비 문안드립니다', '종묘 영녕전', 2, '2023-04-28', '2023-05-01',
+'https://www.chf.or.kr/short/WKTY',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/3ca60146935a44dbaa77797958459e99_1681968436937.jpg', 'Y'
+);
+
+--28
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창덕궁'),
+'행사', '[궁중문화축전] 낭만궁궐 기담극장', '창덕궁 낙선재', 2, '2023-04-30', '2023-05-03',
+'https://www.chf.or.kr/short/fJbM',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/38597ad199ab49929791dfe314cc45ce_1681893999760.jpg', 'Y'
+);
+
+--29
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '[궁중문화축전] 어린이 궁중문화축전-어린이 과거시험', '경복궁', 1, '2023-05-05', '2023-05-07',
+'https://www.chf.or.kr/short/13zK',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/1cff014250fe4423a90d9e949dc65052_1681919786864.png', 'Y'
+);
+
+--30
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창경궁'),
+'행사', '[궁중문화축전] 시간여행-영조, 홍화문을 열다', '창경궁 일원', 2, '2023-05-02', '2023-05-02', 
+'https://www.chf.or.kr/short/s56P',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/8c49541180b34549be3a9bc62538ac95_1681894122416.jpg', 'Y'
+);
+
+--[궁중문화축전] 5대궁 특별관람권 궁패스(경복궁, 창덕궁, 덕수궁, 창경궁, 경희궁)
+
+--31
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '[궁중문화축전] 고궁뮤지컬-세종 1446', '경복궁 근정전', 1, '2023-04-29', '2023-05-02',
+'https://www.chf.or.kr/short/meJH',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/63575d04005748acac86f341a1eab799_1681807795789.jpg', 'Y'
+);
+
+--2023년 봄 궁중문화축전(경복궁, 창덕궁, 덕수궁, 창경궁, 경희궁, 종묘, 사직단)
+
+--32
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창경궁'),
+'행사', '2023년 창경궁 야연', '창경궁 문정전', 1, '2023-05-02', '2023-05-19',
+'https://www.chf.or.kr/short/baHS',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/8476b8c2e2fa4423a6d5964fab705722_1681366886935.jpg', 'Y'
+);
+
+--33
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 경복궁 별빛야행', '경복궁', 2, '2023-04-15', '2023-05-13',
+'https://www.chf.or.kr/short/PUxj',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/859c024c432c4cd99fbf634e60d993e7_1680853042541.jpg', 'Y'
+);
+
+--34
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '덕수궁'),
+'행사', '밤의 석조전 - 오얏꽃 흩날리는 봄밤, 대한제국 황실의 문이 열립니다.', '덕수궁 석조전', 3, '2023-04-11', '2023-05-11',
+'https://www.chf.or.kr/short/8L17',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/dd5c773b90d64273bbbd1e27fdec78c1_1680572403505.png', 'Y'
+);
+
+--35
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창경궁'),
+'행사', '[궁중문화축전] 어명이오! 궁중 새내기 모여라', '창경궁 일원', 3, '2023-05-03', '2023-05-05',
+'https://www.chf.or.kr/short/Lfbp',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202303/0a1be81f991e407ab6cfae620369ca37_1679644836161.png', 'Y'
+);
+
+--36
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 봄 궁중문화축전 개막제', '경복궁(*사전행사 : 창경궁)', 1, '2023-04-28', '2023-04-28',
+'https://www.chf.or.kr/short/w8ai',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/1d69d37344dd4d0f8709deeecccc8595_1681806379953.jpg', 'Y'
+);
+
+--37
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '창덕궁'),
+'행사', '사회적 배려 대상자 초청 ‘창덕궁 달빛기행’ 모집', '창덕궁', 2, '2023-04-29', '2023-04-30', 'https://www.chf.or.kr/short/8EV5',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202304/d7a2388007a845eebdbbea6bec33dc89_1680683883581.png', 'Y'
+);
+
+--38
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023 전국 수문장 임명의식 - 국왕께서 친히 전국의 수문장을 낙점하다!', '경복궁 흥례문 일원', 1, '2023-03-26', '2023-03-26',
+'https://www.chf.or.kr/short/T0tU',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202303/37e8e4bf594449139634a035ff183f0d_1679029333355.jpg', 'N'
+);
+
+insert into event_list values(
+eventlist_id_seq.nextval, 
+(SELECT DISTINCT r.royal_id FROM royal r LEFT JOIN event_list e ON r.royal_id = e.royal_id WHERE r.royal_name = '경복궁'),
+'행사', '2023년 수문장 세화 나눔', '경복궁 광화문 수문장청', 2, '2024-01-21', '2024-01-24', 'https://www.chf.or.kr/short/AcDf',
+'https://www.chf.or.kr/jnrepo/upload/jnPrgCalendarContents/202301/4cbde8fa30384e639ac01add14bb70df_1673934964467.jpg', 'N'
+);
