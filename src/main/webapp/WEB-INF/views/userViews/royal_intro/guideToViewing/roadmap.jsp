6be5bb7b012b7740120ea2170062f846938653ab
joehyunwoo <joehyunwoo080@gmail.com>
2024-02-26 13:36:24 +0900

    🚨 오류 및 수정사항  > 유저app - 관람안내 - 오시는길 전체적인 수정     - 메인타이틀 (오시는길) 추가     - (xxx 오시는 길) 서브 타이틀 추가     - 궁 네비게이션 수정     - 전체적인 디자인 수정     - 전체적인 수정을 하면서 js / css 코드들 정리

--- src/main/webapp/WEB-INF/views/userViews/royal_intro/guideToViewing/roadmap.jsp
+++ src/main/webapp/WEB-INF/views/userViews/royal_intro/guideToViewing/roadmap.jsp
@@ -47,52 +47,59 @@
 					href="/royal/palace/roadmap"><B>오시는 길</a></li>
 			</ul>
 		</div>
-		<div class="right-container">
-			<div class="royal-name">
-				<div class="palace-link GB_BTN" data-lat="37.57962"
-					data-lng="126.977028">
-					<a href="/royal/palace/roadmap">경복궁</a>
-				</div>
-				<div class="palace-link DS_BTN" data-lat="37.565824"
-					data-lng="126.974618">
-					<a href="/royal/palace/roadmap">덕수궁</a>
-				</div>
-				<div class="palace-link CG_BTN" data-lat="37.578758"
-					data-lng="126.995310">
-					<a href="/royal/palace/roadmap">창경궁</a>
-				</div>
-				<div class="palace-link CD_BTN" data-lat="37.579032"
-					data-lng="126.991012">
-					<a href="/royal/palace/roadmap">창덕궁</a>
-				</div>
-				<div class="palace-link JM_BTN" data-lat="37.574574"
-					data-lng="126.994143">
-					<a href="/royal/palace/roadmap">종묘</a>
-				</div>
+		<div class="contents_inn" id="sub_contents">
+
+			<div class="sub_tit_wrap">
+				<h2 class="tit">오시는 길</h2>
 			</div>
+			
+			<div class="container">
+				<div id="royalNames">
+					<div class="palace-link royalname" id="gbBtn_active"
+						data-lat="37.57962" data-lng="126.977028">
+						<input type="hidden" name="royal_id" value="1" />경복궁
+					</div>
+					<div class="palace-link royalname" data-lat="37.565824"
+						data-lng="126.974618">
+						<input type="hidden" name="royal_id" value="4" />덕수궁
+					</div>
+					<div class="palace-link royalname" data-lat="37.578758"
+						data-lng="126.995310">
+						<input type="hidden" name="royal_id" value="3" />창경궁
+					</div>
+					<div class="palace-link royalname" data-lat="37.579032"
+						data-lng="126.991012">
+						<input type="hidden" name="royal_id" value="2" />창덕궁
+					</div>
+					<div class="palace-link royalname" data-lat="37.574574"
+						data-lng="126.994143">
+						<input type="hidden" name="royal_id" value="5" />종묘
+					</div>
+				</div>
+				<div id="white_space"></div>
+			</div>
+
+			<h2 class="subTitle"></h2>
 
 			<div class="map-container">
 				<div class="map"></div>
 				<div class="palace-info" id="경복궁">
-					<div class="address"></div>
-					<div class="phone"></div>
+					<div class="address">
+						<div class="palace-info_title">
+							<i class="fa-solid fa-location-dot" aria-hidden="true"></i>
+							주소
+						</div>
+						<div class="palace-info_content address_content"></div>
+					</div>
+					<div class="phone">
+						<div class="palace-info_title">
+							<i class="fa-solid fa-phone-volume" aria-hidden="true"></i>
+							연락처
+						</div>
+						<div class="palace-info_content phone_content"></div>
+					</div>
 				</div>
-				<div class="palace-info" id="덕수궁">
-					<div class="address"></div>
-					<div class="phone"></div>
-				</div>
-				<div class="palace-info" id="창경궁">
-					<div class="address"></div>
-					<div class="phone"></div>
-				</div>
-				<div class="palace-info" id="창덕궁">
-					<div class="address"></div>
-					<div class="phone"></div>
-				</div>
-				<div class="palace-info" id="종묘">
-					<div class="address"></div>
-					<div class="phone"></div>
-				</div>
+				<div id="palace-info_white_space"></div>
 			</div>
 		</div>
 	</div>
--- src/main/webapp/resources/js/royalInfo/roadmap.js
+++ src/main/webapp/resources/js/royalInfo/roadmap.js
@@ -1,98 +1,108 @@
 // 각 궁 이름에 해당하는 정보를 담은 객체
 const palaceInfo = {
     "경복궁": {
-        "address": "주소 <br>(03045) 서울 종로구 사직로 161",
-        "phone": "연락처 <br>대표전화 02-3700-3900 팩스 02-3700-3909"
+        "address": "(03045) 서울 종로구 사직로 161",
+        "phone": "대표전화 02-3700-3900 팩스 02-3700-3909"
     },
     "창덕궁": {
-        "address": "주소 <br>(03072) 서울 종로구 율곡로 99",
-        "phone": "연락처 <br>대표전화 02-3668-2300 팩스 02-762-2070"
+        "address": "(03072) 서울 종로구 율곡로 99",
+        "phone": "대표전화 02-3668-2300 팩스 02-762-2070"
     },
     "덕수궁": {
-        "address": "주소 <br>(04519) 서울 중구 세종대로 99",
-        "phone": "연락처 <br>대표전화 02-771-9951(사무실) 팩스 02-771-9953"
+        "address": "(04519) 서울 중구 세종대로 99",
+        "phone": "대표전화 02-771-9951(사무실) 팩스 02-771-9953"
     },
     "창경궁": {
-        "address": "주소 <br>(03072) 서울 종로구 창경궁로 185",
-        "phone": "연락처 <br>대표전화 02-762-4868 팩스 02-762-9514"
+        "address": "(03072) 서울 종로구 창경궁로 185",
+        "phone": "대표전화 02-762-4868 팩스 02-762-9514"
     },
     "종묘": {
-        "address": "주소 <br>(03135) 서울 종로구 종로 157",
-        "phone": "연락처 <br>대표전화 02-765-0195 팩스 02-3672-4332"
+        "address": "(03135) 서울 종로구 종로 157",
+        "phone": "대표전화 02-765-0195 팩스 02-3672-4332"
     }
 };
 
-const addressIcon = document.createElement("i"); 
+const addressIcon = document.createElement("i");
 addressIcon.classList.add("fa-solid", "fa-location-dot");
 
-const phoneIcon = document.createElement("i"); 
+const phoneIcon = document.createElement("i");
 phoneIcon.classList.add("fa-solid", "fa-phone-volume");
 
-		
+
 const mapContainer = document.querySelector('.map'); // 지도를 표시할 div
 const mapOption = {
-	center : new kakao.maps.LatLng(37.57962, 126.977028), // 지도의 중심좌표(기본 경복궁)
-	level : 4 // 지도의 확대 레벨
+    center: new kakao.maps.LatLng(37.57962, 126.977028), // 지도의 중심좌표(기본 경복궁)
+    level: 4 // 지도의 확대 레벨
 };
 
 const map = new kakao.maps.Map(mapContainer, mapOption);
 
 const marker = new kakao.maps.Marker({
-	// 지도 중심좌표에 마커를 생성
-	position : map.getCenter()
+    // 지도 중심좌표에 마커를 생성
+    position: map.getCenter()
 });
 // 지도에 마커를 표시
 marker.setMap(map);
 
 // 기본 로드시 경복궁 정보 표시
-const defaultPalace ="경복궁";
-const defaultPalaceInfoDiv = document.getElementById(defaultPalace);
-const defaultPalaceAddress = defaultPalaceInfoDiv.querySelector('.address');
-const defaultPalacePhone = defaultPalaceInfoDiv.querySelector('.phone');
+const defaultPalace = "경복궁";
+const address_content = document.querySelector('.address_content');
+const phone_content = document.querySelector('.phone_content');
 
-const defaultAddressIcon = addressIcon.cloneNode(true);
-const defaultPhoneIcon = phoneIcon.cloneNode(true);
+address_content.innerHTML = palaceInfo[defaultPalace].address;
+phone_content.innerHTML = palaceInfo[defaultPalace].phone;
 
-defaultPalaceAddress.appendChild(defaultAddressIcon);
-defaultPalaceAddress.innerHTML += palaceInfo[defaultPalace].address;
-defaultPalacePhone.appendChild(defaultPhoneIcon);
-defaultPalacePhone.innerHTML += palaceInfo[defaultPalace].phone;
+// xxx오시는 길 삽입
+var subTitle = document.querySelector('.subTitle');
+subTitle.innerHTML = '경복궁 오시는 길';
 
 // 각 palace-link 클래스를 가진 요소에 대해 클릭 이벤트 리스너 추가
-document.querySelectorAll('.palace-link').forEach( (link)=> {
-	link.addEventListener('click', (event) => {
-		event.preventDefault();
+document.querySelectorAll('.palace-link').forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
 
-		const lat = parseFloat(link.getAttribute('data-lat'));
-		const lng = parseFloat(link.getAttribute('data-lng'));
+        const lat = parseFloat(link.getAttribute('data-lat'));
+        const lng = parseFloat(link.getAttribute('data-lng'));
 
-		// 새로운 지도 중심 좌표 생성
-		const newCenter = new kakao.maps.LatLng(lat, lng);
-		marker.setPosition(newCenter);
-		map.setCenter(newCenter);
+        // 새로운 지도 중심 좌표 생성
+        const newCenter = new kakao.maps.LatLng(lat, lng);
+        marker.setPosition(newCenter);
+        map.setCenter(newCenter);
 
-		// 클릭된 궁의 이름 가져오기
-		const palaceName = link.textContent.trim();
-		
-		 // 모든 궁 정보 요소를 숨김
-        document.querySelectorAll('.palace-info').forEach((info) => {
-            info.style.display = 'none';
-        });
+        // 클릭된 궁의 이름 가져오기
+        const palaceName = link.textContent.trim();
 
-        // 클릭된 궁의 정보 요소를 표시
-        const palaceInfoDiv = document.getElementById(palaceName);
-        palaceInfoDiv.style.display = 'block';
+        // 클릭된 궁 정보 표시
+        address_content.innerHTML = palaceInfo[palaceName].address;
+        phone_content.innerHTML = palaceInfo[palaceName].phone;
 
-        const palaceInfoAddress = palaceInfoDiv.querySelector('.address');
-        const palaceInfoPhone = palaceInfoDiv.querySelector('.phone');
-
-        palaceInfoAddress.innerHTML = '';
-        palaceInfoPhone.innerHTML = '';
-
-        palaceInfoAddress.appendChild(addressIcon);
-		palaceInfoAddress.insertAdjacentHTML('beforeend', palaceInfo[palaceName].address);
-		palaceInfoPhone.appendChild(phoneIcon);
-		palaceInfoPhone.insertAdjacentHTML('beforeend', palaceInfo[palaceName].phone);
-	});
+        // xxx오시는 길 삽입
+        subTitle.innerHTML = `${palaceName} 오시는 길`;
+    });
 });
 
+// 궁 클릭 유지
+const palaceNames = document.querySelectorAll(".royalname");
+for (const palaceName of palaceNames) {
+    palaceName.addEventListener("click", (e) => {
+
+        for (const palaceName of palaceNames) {
+            if (e.currentTarget == palaceName) {
+                var innerText = e.currentTarget.innerText;
+                if (innerText == '경복궁') {
+                    e.target.id = 'gbBtn_active';
+                } else if (innerText == '덕수궁') {
+                    e.target.id = 'dsBtn_active';
+                } else if (innerText == '창경궁') {
+                    e.target.id = 'chBtn_active';
+                } else if (innerText == '창덕궁') {
+                    e.target.id = 'cdBtn_active';
+                } else if (innerText == '종묘') {
+                    e.target.id = 'gmBtn_active';
+                }
+            } else {
+                palaceName.id = '';
+            }
+        }
+    })
+}
